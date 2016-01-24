afcfb5428ee35fd8bed68d216f096f4ecd0d82ec
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2016-01-24 21:47:20 +0300

    alert

--- test/frontend/src/main/scala/com/karasiq/bootstrap/test/frontend/TestProgressBar.scala
+++ test/frontend/src/main/scala/com/karasiq/bootstrap/test/frontend/TestProgressBar.scala
@@ -2,6 +2,7 @@ package com.karasiq.bootstrap.test.frontend
 
 import com.karasiq.bootstrap.BootstrapComponent
 import com.karasiq.bootstrap.BootstrapImplicits._
+import com.karasiq.bootstrap.alert.{Alert, AlertStyle}
 import com.karasiq.bootstrap.progressbar.{ProgressBar, ProgressBarStyle}
 import rx._
 import rx.ops._
@@ -22,8 +23,13 @@ final class TestProgressBar(style: ProgressBarStyle, updateInterval: FiniteDurat
       if (progressBarValue.now < 100) {
         progressBarValue.update(progressBarValue.now + 1)
       } else {
-        progressBar.parentNode.removeChild(progressBar)
+        val alert = Alert(AlertStyle.success,
+          strong("Testing"), " has finished. ",
+          Alert.link(href := "https://getbootstrap.com/components/#alerts", target := "blank", "Alert inline link.")
+        )
+        progressBar.parentNode.replaceChild(alert.render, progressBar)
         progressBarValue.killAll()
+        timer.killAll()
       }
     }
 
