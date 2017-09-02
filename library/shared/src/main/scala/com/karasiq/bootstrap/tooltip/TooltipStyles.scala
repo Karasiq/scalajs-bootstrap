3df107f1c8c0445a7e67520bf0294dc852bb6611
Karasiq <yoba123@yandex.ru>
2017-09-02 13:16:22 +0300

    Tooltip styles

---
+++ library/shared/src/main/scala/com/karasiq/bootstrap/tooltip/TooltipStyles.scala
@@ -0,0 +1,21 @@
+package com.karasiq.bootstrap.tooltip
+
+import com.karasiq.bootstrap.context.RenderingContext
+
+trait TooltipStyles { self: RenderingContext ⇒
+  import scalaTags.all._
+
+  final class TooltipPlacement private[tooltip](val placement: String) {
+    override def toString: String = placement
+  }
+
+  //noinspection TypeAnnotation
+  object TooltipPlacement {
+    private[this] def placement(str: String): TooltipPlacement = new TooltipPlacement(str)
+    val auto = placement("auto")
+    val left = placement("left")
+    val right = placement("right")
+    val top = placement("top")
+    val bottom = placement("bottom")
+  }
+}
