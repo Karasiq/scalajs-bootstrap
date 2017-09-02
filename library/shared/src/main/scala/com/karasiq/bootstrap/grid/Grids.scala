ea0169e9120b549a4c2c86da6d0c0f57988277e9
Karasiq <yoba123@yandex.ru>
2017-09-02 09:42:57 +0300

    Abstract grids

--- library/shared/src/main/scala/com/karasiq/bootstrap/grid/Grids.scala
+++ library/shared/src/main/scala/com/karasiq/bootstrap/grid/Grids.scala
@@ -1,91 +1,26 @@
 package com.karasiq.bootstrap.grid
 
-import com.karasiq.bootstrap.context.BootstrapBundle
-
 import scala.language.{implicitConversions, postfixOps}
 
-trait Grids { self: BootstrapBundle ⇒
+import com.karasiq.bootstrap.context.RenderingContext
+
+trait Grids { self: RenderingContext ⇒
   import scalaTags.all._
 
-  /**
-    * @see [[https://getbootstrap.com/css/#grid-options]]
-    */
-  object GridSystem {
-    lazy val container: Tag = div(`class` := "container")
-    lazy val containerFluid: Tag = div(`class` := "container-fluid")
-    lazy val row: Tag = div(`class` := "row")
+  trait AbstractGridSystem {
+    type ContainerT = Tag
+    type RowT = Tag
+
+    def container: ContainerT
+    def containerFluid: ContainerT
+    def row: RowT
+
+    def col: AbstractColumnFactory
+    def hidden: AbstractGridVisibilityFactory
+    def visible: AbstractGridVisibilityFactory
 
     def mkRow(md: Modifier*): Tag = {
       row(col(col.maxSize).asDiv(md))
-    }
-
-    object col {
-      val minSize: Int = 1
-      val maxSize: Int = 12
-
-      sealed trait GridColSize extends ModifierFactory {
-        final def asDiv: Tag = div(this.createModifier)
-      }
-
-      @inline
-      private[this] def singleColSize(modifier: String, size: Int): GridColSize = new GridColSize {
-        // require(modifier.nonEmpty && size <= 12 && size > 0, "Invalid grid column properties")
-        override val createModifier: ModifierT = ("col-" + modifier + "-" + size).addClass
-      }
-      
-      def xs(size: Int): GridColSize = singleColSize("xs", size)
-      def sm(size: Int): GridColSize = singleColSize("sm", size)
-      def md(size: Int): GridColSize = singleColSize("md", size)
-      def lg(size: Int): GridColSize = singleColSize("lg", size)
-
-      def responsive(xsSize: Int, smSize: Int, mdSize: Int, lgSize: Int): GridColSize = new GridColSize {
-        override val createModifier: ModifierT = Seq[ModifierT](xs(xsSize), sm(smSize), md(mdSize), lg(lgSize))
-      }
-
-      def apply(size: Int): GridColSize = this.responsive(size, size, size, size)
-    }
-
-    /**
-      * @see [[http://getbootstrap.com/css/#responsive-utilities]]
-      */
-    object hidden {
-      final class GridHiddenModifier private[grid](size: String) extends ModifierFactory {
-        val createModifier: ModifierT = ("hidden-" + size).addClass
-      }
-
-      @inline
-      private[this] implicit def gridHidden(size: String): GridHiddenModifier = new GridHiddenModifier(size)
-
-      lazy val xs: GridHiddenModifier = "xs"
-      lazy val sm: GridHiddenModifier = "sm"
-      lazy val md: GridHiddenModifier = "md"
-      lazy val lg: GridHiddenModifier = "lg"
-      lazy val print: GridHiddenModifier = "print"
-    }
-
-    /**
-      * @see [[http://getbootstrap.com/css/#responsive-utilities]]
-      */
-    object visible {
-      final class GridVisibleModifier private[grid](size: String, as: String) extends ModifierFactory {
-        val createModifier: ModifierT = ("visible-" + size + "-" + as).addClass
-      }
-
-      final class GridVisibility private[grid](size: String) extends ModifierFactory {
-        lazy val block = new GridVisibleModifier(size, "block")
-        lazy val inline = new GridVisibleModifier(size, "inline")
-        lazy val `inline-block` = new GridVisibleModifier(size, "inline-block")
-        lazy val createModifier: Modifier = block.createModifier // Default
-      }
-
-      @inline
-      private[this] implicit def gridVisibility(size: String): GridVisibility = new GridVisibility(size)
-
-      lazy val xs: GridVisibility = "xs"
-      lazy val sm: GridVisibility = "sm"
-      lazy val md: GridVisibility = "md"
-      lazy val lg: GridVisibility = "lg"
-      lazy val print: GridVisibility = "print"
     }
 
     def visibility(xs: Boolean = true, sm: Boolean = true, md: Boolean = true, lg: Boolean = true): Modifier = {
@@ -96,6 +31,42 @@ trait Grids { self: BootstrapBundle ⇒
         if (lg) visible.lg else hidden.lg
       )
     }
+  }
+
+  trait AbstractColumn extends ModifierFactory {
+    def size: Int
+    def asDiv: Tag
+  }
+
+  trait AbstractColumnFactory {
+    def minSize: Int
+    def maxSize: Int
+
+    def xs(size: Int): AbstractColumn
+    def sm(size: Int): AbstractColumn
+    def md(size: Int): AbstractColumn
+    def lg(size: Int): AbstractColumn
+
+    def responsive(xsSize: Int, smSize: Int, mdSize: Int, lgSize: Int): AbstractColumn = new AbstractColumn {
+      override val createModifier: ModifierT = Seq[ModifierT](xs(xsSize), sm(smSize), md(mdSize), lg(lgSize))
+      def size: Int = lgSize
+      def asDiv: Tag = div(createModifier)
+    }
+
+    def apply(size: Int): AbstractColumn = this.responsive(size, size, size, size)
+  }
+
+  trait AbstractGridVisibility extends ModifierFactory {
+    def screenSize: String
+    def hidden: Boolean
+  }
+
+  trait AbstractGridVisibilityFactory {
+    def xs: AbstractGridVisibility
+    def sm: AbstractGridVisibility
+    def md: AbstractGridVisibility
+    def lg: AbstractGridVisibility
+    def print: AbstractGridVisibility
   }
 }
 
---
+++ library/shared/src/main/scala/com/karasiq/bootstrap/grid/UniversalGrids.scala
@@ -0,0 +1,87 @@
+package com.karasiq.bootstrap.grid
+
+import scala.language.implicitConversions
+
+import com.karasiq.bootstrap.context.RenderingContext
+import com.karasiq.bootstrap.utils.ClassModifiers
+
+trait UniversalGrids { self: RenderingContext with Grids with ClassModifiers ⇒
+  import scalaTags.all._
+
+  /**
+    * @see [[https://getbootstrap.com/css/#grid-options]]
+    */
+  object GridSystem extends AbstractGridSystem {
+    val container: Tag = div(`class` := "container")
+    val containerFluid: Tag = div(`class` := "container-fluid")
+    val row: Tag = div(`class` := "row")
+
+    object col extends AbstractColumnFactory {
+      val minSize: Int = 1
+      val maxSize: Int = 12
+
+      sealed trait GridColSize extends AbstractColumn {
+        final def asDiv: Tag = div(this.createModifier)
+      }
+
+      @inline
+      private[this] def singleColSize(screenSize: String, _size: Int): GridColSize = new GridColSize {
+        // require(modifier.nonEmpty && size <= 12 && size > 0, "Invalid grid column properties")
+        override val createModifier: ModifierT = ("col-" + screenSize + "-" + _size).addClass
+        override val size: Int = _size
+      }
+      
+      def xs(size: Int): GridColSize = singleColSize("xs", size)
+      def sm(size: Int): GridColSize = singleColSize("sm", size)
+      def md(size: Int): GridColSize = singleColSize("md", size)
+      def lg(size: Int): GridColSize = singleColSize("lg", size)
+    }
+
+    /**
+      * @see [[http://getbootstrap.com/css/#responsive-utilities]]
+      */
+    object hidden extends AbstractGridVisibilityFactory {
+      final class UniversalGridHiddenModifier private[grid](val screenSize: String) extends AbstractGridVisibility {
+        val hidden = true
+        val createModifier: ModifierT = ("hidden-" + screenSize).addClass
+      }
+
+      @inline
+      private[this] implicit def gridHidden(size: String): UniversalGridHiddenModifier = {
+        new UniversalGridHiddenModifier(size)
+      }
+
+      lazy val xs: UniversalGridHiddenModifier = "xs"
+      lazy val sm: UniversalGridHiddenModifier = "sm"
+      lazy val md: UniversalGridHiddenModifier = "md"
+      lazy val lg: UniversalGridHiddenModifier = "lg"
+      lazy val print: UniversalGridHiddenModifier = "print"
+    }
+
+    /**
+      * @see [[http://getbootstrap.com/css/#responsive-utilities]]
+      */
+    object visible extends AbstractGridVisibilityFactory {
+      class UniversalGridVisibleModifier private[grid](val screenSize: String, as: String) extends AbstractGridVisibility {
+        val hidden = false
+        val createModifier: ModifierT = ("visible-" + screenSize + "-" + as).addClass
+      }
+
+      final class UniversalGridVisibility private[grid](screenSize: String) extends UniversalGridVisibleModifier(screenSize, "block") {
+        val block: UniversalGridVisibleModifier = this
+        lazy val inline = new UniversalGridVisibleModifier(screenSize, "inline")
+        lazy val `inline-block` = new UniversalGridVisibleModifier(screenSize, "inline-block")
+      }
+
+      @inline
+      private[this] implicit def gridVisibility(size: String): UniversalGridVisibility = new UniversalGridVisibility(size)
+
+      lazy val xs: UniversalGridVisibility = "xs"
+      lazy val sm: UniversalGridVisibility = "sm"
+      lazy val md: UniversalGridVisibility = "md"
+      lazy val lg: UniversalGridVisibility = "lg"
+      lazy val print: UniversalGridVisibility = "print"
+    }
+  }
+}
+
