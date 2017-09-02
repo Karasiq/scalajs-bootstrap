3ba3dba87ed6fb273009f057c637a6cc77dd202c
Karasiq <yoba123@yandex.ru>
2017-09-02 11:49:12 +0300

    Abstract carousels

--- library/shared/src/main/scala/com/karasiq/bootstrap/carousel/TextCarousels.scala
+++ library/shared/src/main/scala/com/karasiq/bootstrap/carousel/TextCarousels.scala
@@ -1,22 +1,25 @@
 package com.karasiq.bootstrap.carousel
 
-import com.karasiq.bootstrap.context.RenderingContext
-import com.karasiq.bootstrap.utils.Utils
-import rx.Rx
-
 import scala.language.postfixOps
 
-trait TextCarousels { self: Carousels with RenderingContext with Utils ⇒
+import rx.Rx
+
+import com.karasiq.bootstrap.components.BootstrapComponents
+import com.karasiq.bootstrap.context.RenderingContext
+import com.karasiq.bootstrap.icons.Icons
+import com.karasiq.bootstrap.utils.Utils
+
+trait TextCarousels extends UniversalCarousels { self: RenderingContext with Carousels with Utils with Icons with BootstrapComponents ⇒
   import scalaTags.all._
 
-  /**
-    * A slideshow component for cycling through elements, like a carousel.
-    * @note Nested carousels are not supported.
-    * @see [[http://getbootstrap.com/javascript/#carousel]]
-    */
+  type Carousel = UniversalCarousel
   object Carousel extends CarouselFactory {
-    def apply(data: Rx[Seq[Modifier]], id: String = Bootstrap.newId): Carousel = {
-      new Carousel(id, data)
+    def apply(data: Rx[Seq[Modifier]], id: String = Bootstrap.newId): UniversalCarousel = {
+      new UniversalCarousel(id, data)
+    }
+
+    def slide(image: String, content: Modifier*): Modifier = {
+      UniversalCarousel.slide(image, content)
     }
   }
 }
