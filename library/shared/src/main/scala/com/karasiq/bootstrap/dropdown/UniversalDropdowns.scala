112998bfdb610f59de9d426b325e02ab29b2c3f7
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2017-09-02 11:19:45 +0300

    Abstract dropdowns

---
+++ library/shared/src/main/scala/com/karasiq/bootstrap/dropdown/UniversalDropdowns.scala
@@ -0,0 +1,45 @@
+package com.karasiq.bootstrap.dropdown
+
+import com.karasiq.bootstrap.context.RenderingContext
+import com.karasiq.bootstrap.utils.Utils
+
+trait UniversalDropdowns { self: RenderingContext with Dropdowns with Utils ⇒
+  import scalaTags.all._
+
+  import BootstrapAttrs._
+
+  type Dropdown = AbstractDropdown
+  object Dropdown extends DropdownFactory {
+    def link(target: String, md: Modifier*): Tag = {
+      li(a(href := target, md))
+    }
+
+    def apply(title: Modifier, items: Modifier*): Dropdown = {
+      new UniversalDropdown(title, items)
+    }
+  }
+
+  private[dropdown] final class UniversalDropdown(val title: Modifier,
+                                                  val items: Seq[Modifier],
+                                                  val dropdownId: String = Bootstrap.newId)
+    extends AbstractDropdown {
+
+    val dropdown: Tag = {
+      div(`class` := "dropdown")(
+        Bootstrap.button(
+          "dropdown-toggle".addClass,
+          id := dropdownId,
+          `data-toggle` := "dropdown",
+          aria.haspopup := true,
+          aria.expanded := false,
+          title,
+          Bootstrap.nbsp,
+          span(`class` := "caret")
+        ),
+        ul(`class` := "dropdown-menu", aria.labelledby := dropdownId)(items:_*)
+      )
+    }
+
+    def dropup: Tag = this.dropdown(`class` := "dropup")
+  }
+}
