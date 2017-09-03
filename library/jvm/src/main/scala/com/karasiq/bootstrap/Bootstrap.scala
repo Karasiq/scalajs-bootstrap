4ecb31a8993b6e5aff67372f9b1bdbf498d5bd2e
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2017-09-03 08:13:30 +0300

    Safe Rx context

--- library/jvm/src/main/scala/com/karasiq/bootstrap/Bootstrap.scala
+++ library/jvm/src/main/scala/com/karasiq/bootstrap/Bootstrap.scala
@@ -1,13 +1,14 @@
 package com.karasiq.bootstrap
 
-import com.karasiq.bootstrap.context.TextBootstrapBundle
 import rx._
+
+import com.karasiq.bootstrap.context.TextBootstrapBundle
 
 /**
   * Global context
   */
 object Bootstrap {
-  private[this] lazy val _scalaRxContext = Ctx.Owner.Unsafe
+  private[this] lazy val _scalaRxContext = Ctx.Owner.safe()
 
   lazy val text: TextBootstrapBundle = new TextBootstrapBundle {
     implicit val scalaRxContext = _scalaRxContext
