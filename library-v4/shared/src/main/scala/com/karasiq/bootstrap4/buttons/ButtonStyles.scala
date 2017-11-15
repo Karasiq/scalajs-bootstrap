84c970ad7efb84b67d9d3f6a78dc29e4e579330c
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2017-11-15 04:24:46 +0300

    Button styles

--- library-v4/shared/src/main/scala/com/karasiq/bootstrap4/buttons/ButtonStyles.scala
+++ library-v4/shared/src/main/scala/com/karasiq/bootstrap4/buttons/ButtonStyles.scala
@@ -18,32 +18,46 @@ trait ButtonStyles { self: RenderingContext with Utils ⇒
   }
 
   /**
-    * @see [[https://getbootstrap.com/css/#buttons-sizes]]
+    * @see [[https://getbootstrap.com/docs/4.0/components/buttons/#sizes]]
     */
   object ButtonSize {
     def default = DefaultButtonSize
     lazy val large = new ButtonSizeValue("lg")
     lazy val small = new ButtonSizeValue("sm")
-    lazy val extraSmall = new ButtonSizeValue("xs")
   }
-
-
-
-  final class ButtonStyle private[buttons](style: String) extends ModifierFactory {
+  
+  class ButtonStyle private[buttons](style: String) extends ModifierFactory {
     val className = s"btn-$style"
     val createModifier = className.addClass
   }
 
   /**
-    * @see [[https://getbootstrap.com/css/#buttons-options]]
+    * @see [[https://getbootstrap.com/docs/4.0/components/buttons/]]
     */
   object ButtonStyle {
-    lazy val default = new ButtonStyle("default")
+    def default = primary 
     lazy val primary = new ButtonStyle("primary")
+    lazy val secondary = new ButtonStyle("secondary")
     lazy val success = new ButtonStyle("success")
     lazy val info = new ButtonStyle("info")
     lazy val warning = new ButtonStyle("warning")
     lazy val danger = new ButtonStyle("danger")
+    lazy val light = new ButtonStyle("light")
+    lazy val dark = new ButtonStyle("dark")
     lazy val link = new ButtonStyle("link")
+  }
+
+  class ButtonOutline private[buttons](style: String) extends ButtonStyle(s"outline-$style")
+
+  object ButtonOutline {
+    lazy val primary = new ButtonOutline("primary")
+    lazy val secondary = new ButtonOutline("secondary")
+    lazy val success = new ButtonOutline("success")
+    lazy val info = new ButtonOutline("info")
+    lazy val warning = new ButtonOutline("warning")
+    lazy val danger = new ButtonOutline("danger")
+    lazy val light = new ButtonOutline("light")
+    lazy val dark = new ButtonOutline("dark")
+    lazy val white = new ButtonOutline("white")
   }
 }
