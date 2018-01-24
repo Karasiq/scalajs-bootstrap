fc677f12914ec6b2a29c3db416bf105ec0b4c3a9
Karasiq <yoba123@yandex.ru>
2018-01-24 09:39:28 +0300

    Shared bootstrap-jquery library

---
+++ jquery/src/main/scala/com/karasiq/bootstrap/jquery/BootstrapJQuery.scala
@@ -0,0 +1,15 @@
+package com.karasiq.bootstrap.jquery
+
+import scala.language.postfixOps
+import scala.scalajs.js
+
+import org.scalajs.jquery.JQuery
+
+@scalajs.js.native
+trait BootstrapJQuery extends scalajs.js.Object {
+  def tab(options: scalajs.js.Any): JQuery = scalajs.js.native
+  def carousel(options: scalajs.js.Any = ???): JQuery = scalajs.js.native
+  def modal(options: scalajs.js.Any = ???): JQuery = scalajs.js.native
+  def tooltip(options: scalajs.js.Any = ???): JQuery = scalajs.js.native
+  def popover(options: scalajs.js.Any = ???): JQuery = js.native
+}
