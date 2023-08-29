7a6631590c44deb3a62033c49aea7c14fbee1808
Scala Steward <scala_steward@virtuslab.com>
2023-08-29 18:26:35 +0000

    Reformat with scalafmt 3.7.13
    Executed command: scalafmt --non-interactive

--- library-v4/shared/src/main/scala/com/karasiq/bootstrap4/table/TableStyles.scala
+++ library-v4/shared/src/main/scala/com/karasiq/bootstrap4/table/TableStyles.scala
@@ -6,17 +6,17 @@ import com.karasiq.bootstrap4.utils.Utils
 trait TableStyles { self: RenderingContext with Utils ⇒
   import scalaTags.all._
 
-  final class TableStyle private[table](val styleName: String) extends StyleClassModifier {
-    val className = s"table-$styleName"
+  final class TableStyle private[table] (val styleName: String) extends StyleClassModifier {
+    val className      = s"table-$styleName"
     val createModifier = className.addClass
   }
 
   object TableStyle {
-    lazy val dark = new TableStyle("dark")
-    lazy val striped = new TableStyle("striped")
-    lazy val hover = new TableStyle("hover")
-    lazy val bordered = new TableStyle("bordered")
-    lazy val small = new TableStyle("sm")
+    lazy val dark       = new TableStyle("dark")
+    lazy val striped    = new TableStyle("striped")
+    lazy val hover      = new TableStyle("hover")
+    lazy val bordered   = new TableStyle("bordered")
+    lazy val small      = new TableStyle("sm")
     lazy val borderless = new TableStyle("borderless")
   }
 }
--- library/shared/src/main/scala/com/karasiq/bootstrap/alert/AlertStyles.scala
+++ library/shared/src/main/scala/com/karasiq/bootstrap/alert/AlertStyles.scala
@@ -5,14 +5,14 @@ import com.karasiq.bootstrap.context.RenderingContext
 trait AlertStyles { self: RenderingContext ⇒
   import scalaTags.all._
 
-  final class AlertStyle private[alert](val styleName: String) extends ModifierFactory {
+  final class AlertStyle private[alert] (val styleName: String) extends ModifierFactory {
     val createModifier = s"alert-$styleName".addClass
   }
 
   object AlertStyle {
     lazy val success = new AlertStyle("success")
-    lazy val info = new AlertStyle("info")
+    lazy val info    = new AlertStyle("info")
     lazy val warning = new AlertStyle("warning")
-    lazy val danger = new AlertStyle("danger")
+    lazy val danger  = new AlertStyle("danger")
   }
 }
